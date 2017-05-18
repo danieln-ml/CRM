612b55f61ec1f57454cfddc13e0ac3b06f96df11
Dave McCarthy <davemccarthy@objectlabs.com>
2017-05-18 10:51:34 -0700

    more styling

--- src/js/components/Contacts/ContactList.jsx
+++ src/js/components/Contacts/ContactList.jsx
@@ -4,17 +4,19 @@ export default class ContactList extends React.Component {
 
   render() {
     return (
-      <section className={this.props.className}>
-        <h3>Contacts</h3>
-        { this.props.selectedContact._id &&
-          <button onClick={this.props.addContactAction}>Add Contact</button>
-        }
+      <section className={`contact-list ${this.props.className}`}>
+        <div className="contact-list-top">
+          <h3 className="contact-list-top--title">Contacts</h3>
+          { this.props.selectedContact._id &&
+            <button className="btn btn-primary btn-sm" onClick={this.props.addContactAction}>+ Add Contact</button>
+          }
+        </div>
 
-        <ul className="contact-list">
+        <ul className="contact-list--list">
           { this.props.contacts.length ?
             this.props.contacts.map((contact) => this.renderListItem(contact))
             :
-            <li className="contact-list--item m-empty">You haven't added any contacts, yet.</li>
+            <li className="contact-list--list-item m-empty">You haven't added any contacts, yet.</li>
           }
         </ul>
       </section>
@@ -29,7 +31,7 @@ export default class ContactList extends React.Component {
     return (
       <li
         key={_id}
-        className={`contact-list--item ${selectedClass}`}
+        className={`contact-list--list-item ${selectedClass}`}
         onClick={(e) => this.props.handleSelectContact(_id)}>
         {`${firstName} ${lastName}`}
       </li>
