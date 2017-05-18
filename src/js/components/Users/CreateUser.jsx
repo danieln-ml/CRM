49e64521f4be32db27c582e51b35df52864b3bfc
Daniel Nakashima <daniel@objectlabs.com>
2017-05-18 14:09:06 -0700

    missing added files

---
+++ src/js/components/Users/CreateUser.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Input from "../Inputs/Input.jsx"
+import Form from "../Base/Form.jsx"
+
+export default class Create extends React.Component {
+
+  render() {
+    const {onCreate, children} = this.props
+
+    return (
+      <section className="col-md-6 col-md-offset-3">
+        <h3>Sign Up</h3>
+        <Form onSubmit={onCreate}>
+          {children}
+          <input type="submit" className="btn btn-primary pull-right" value="Create Account" />
+        </Form>
+      </section>
+    )
+  }
+}
